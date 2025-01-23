721ecb592b4e6505f5eee2dd60032e2825ebbce1
DPartney <129333259+DPartney@users.noreply.github.com>
2025-01-23 10:40:38 -0700

    Deck

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,7 +1,14 @@
+using System;
 using UnityEngine;
 
 public class Card : MonoBehaviour
 {
     public int Type;
     public string Description;
+
+    public Card(int type, string description)
+    {
+        Type = type;
+        Description = description;
+    }
 }
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Deck : MonoBehaviour
+{
+    public List<Card> deck;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

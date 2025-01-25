f783bae5b7e214f6e497a508b749bf2c19dbf819
DPartney <129333259+DPartney@users.noreply.github.com>
2025-01-25 16:28:42 -0700

    UI Manager Creation

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class UIManager
+{
+    //public Asset board;
+    //public Asset playerHand;
+    public Card chosencard;
+
+    public void TransitionPlayerScene()
+    {
+
+    }
+
+    public Pawn SelectPawn()
+    {
+        return null;
+    }
+
+    public void SelectCard()
+    {
+
+    }
+
+    public void SelectBoardSpace()
+    {
+        
+    }
+}

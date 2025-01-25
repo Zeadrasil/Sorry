d207b433c66bcb95ac3fb9c9b017523a8ad0fce4
Zeadrasil <thewitepheonix@gmail.com>
2025-01-25 16:57:43 -0700

    Code for Hand.cs

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 public class Hand
 {
     public List<Card> cards;
+    public Deck deck;
 
     public void AddCard()
     {
-        
+        //cards.Add(deck.DrawCard());
     }
 
-    public void DiscardCard(int CardAmount)
+    public void DiscardCard(int cardIndex)
     {
-        
+        if(cardIndex > 3)
+        {
+            cards.Clear();
+        }
+        else
+        {
+            cards.RemoveAt(cardIndex);
+        }
     }
 }
